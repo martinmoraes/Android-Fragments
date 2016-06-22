5323e6973255307f42041d10d1a9b4330424afb0
Martin Morães <martin.moraes@gmail.com>
2016-06-22 11:00:12 -0300

    Implementado esconder os fragments quando activity é recriada.

--- app/src/main/java/br/com/appviral/fragments/MainActivity.java
+++ app/src/main/java/br/com/appviral/fragments/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.appviral.fragments;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.res.Configuration;
@@ -9,13 +10,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements BlankFragment.OnFragmentInteractionListener, View.OnClickListener {
 
-    private Button mB1, mB2, mB3, mB4;
     private int sequencial = 0;
     private BlankFragment mBlankFragment = null;
     private String mTagFragment = null;
@@ -25,32 +24,27 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mB1 = (Button) findViewById(R.id.b1);
-        mB2 = (Button) findViewById(R.id.b2);
-        mB3 = (Button) findViewById(R.id.b3);
-        mB4 = (Button) findViewById(R.id.b4);
+        Button B1 = (Button) findViewById(R.id.b1);
+        Button B2 = (Button) findViewById(R.id.b2);
+        Button B3 = (Button) findViewById(R.id.b3);
+        Button B4 = (Button) findViewById(R.id.b4);
 
-        mB1.setOnClickListener(this);
-        mB2.setOnClickListener(this);
-        mB3.setOnClickListener(this);
-        mB4.setOnClickListener(this);
+        B1.setOnClickListener(this);
+        B2.setOnClickListener(this);
+        B3.setOnClickListener(this);
+        B4.setOnClickListener(this);
 
         modoAparelho();
 
         if (savedInstanceState != null) {
-            mTagFragment = savedInstanceState.getString("TAG");
-
-            if (mTagFragment == null)
-                abreFragments("B1");
-            else
-                abreFragments(mTagFragment);
-        } else{
+            mTagFragment = savedInstanceState.getString("TAG", "B1");
+            escondeTodosFragments(mTagFragment);
+        } else {
             abreFragments("B1");
         }
     }
 
     private void abreFragments(String tag) {
-        // FRAGMENT
         Log.d("MEUAPP", "Está em abreFragments(): " + tag);
 
         FragmentManager fragmentManager = getFragmentManager();
@@ -60,7 +54,7 @@ public class MainActivity extends AppCompatActivity
             ft.hide(mBlankFragment);
         BlankFragment frag = (BlankFragment) fragmentManager.findFragmentByTag(tag);
         if (frag == null) {
-            frag = frag.newInstance(this.toString(), sequencial++);
+            frag = BlankFragment.newInstance(this.toString(), sequencial++);
 //            ft.replace(R.id.rl_fragment_container, frag, tag);
             ft.add(R.id.rl_fragment_container, frag, tag);
         } else {
@@ -73,6 +67,25 @@ public class MainActivity extends AppCompatActivity
         mBlankFragment = frag;
     }
 
+    private void escondeTodosFragments(String TagAtiva) {
+        ArrayList<String> fragmentsTag = new ArrayList<>();
+        fragmentsTag.add("B1");
+        fragmentsTag.add("B2");
+        fragmentsTag.add("B3");
+        fragmentsTag.add("B4");
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+
+        for (String tag : fragmentsTag) {
+            if (!tag.equals(TagAtiva)) {
+                Fragment frag = fragmentManager.findFragmentByTag(tag);
+                if (frag != null) {
+                    ft.hide(frag);
+                }
+            }
+        }
+        ft.commit();
+    }
 
     @Override
     public void onFragmentInteraction(String msg) {
